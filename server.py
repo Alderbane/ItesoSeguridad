148d1208779b7cab684ed059e30fd73b37e8b9e0
Alderbane <alderban@outlook.com>
2022-05-09 22:07:51 -0500

    Signed

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
+from AES256 import AESCipher
+from nacl.signing import SigningKey
+import socket,nacl.secret,nacl.utils
 
-import socket
+key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+aes = AESCipher(key)
+
+
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -12,7 +20,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
+            data = data.decode('utf-8')
+            encrypted = aes.encrypt(data)
+            
+            signing_key = SigningKey.generate()
+            signed = signing_key.sign(encrypted)
+            conn.sendall(signed)
             if not data:
                 break
-            conn.sendall(data)
-            print(data)
+            
