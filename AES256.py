3859765b85f7343d0ee590a15f816c0c69ef7aba
Alderbane <alderban@outlook.com>
2022-05-15 14:16:35 -0500

    Cambiado de CBC a CFB

--- AES256.py
+++ AES256.py
@@ -12,7 +12,7 @@ class AESCipher(object):
     def encrypt(self, raw):
         raw = self._pad(raw)
         iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        cipher = AES.new(self.key, AES.MODE_CFB, iv)
         return base64.b64encode(iv + cipher.encrypt(raw.encode()))
 
     def decrypt(self, enc):
