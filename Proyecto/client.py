3279a6799144fdaf7b576b5e12d916fb8496a56b
Alderbane <alderban@outlook.com>
2022-05-15 14:55:49 -0500

    Moved files to individual folder
    Added logs and key saving

---
+++ Proyecto/client.py
@@ -0,0 +1,19 @@
+import socket, sys
+
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+file = open('./message.txt', 'rb')
+data = file.read() 
+
+eFile = open('./encrypted.txt', 'wb')
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(data)
+    data = s.recv(1024)
+    print(data)
+    eFile.write(data)
+    
---
+++ Proyecto/server.py
@@ -0,0 +1,38 @@
+from Proyecto.AES256 import AESCipher
+from nacl.signing import SigningKey
+import socket,nacl.secret,nacl.utils
+
+key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+aes = AESCipher(key)
+
+
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+
+privFile = open('./priv.key', 'wb')
+pubFile = open('./pub.key', 'wb')
+logsFile = open('./logs.txt', 'a')
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            data = data.decode('utf-8')
+            encrypted = aes.encrypt(data)
+            
+            signing_key = SigningKey.generate()
+            signed = signing_key.sign(encrypted)
+            privFile.write(signing_key._signing_key)
+            pubFile.write(signing_key.verify_key._key)
+
+            logsFile.write("Uploaded file by client\n")
+
+            conn.sendall(signed)
+            if not data:
+                break
+            
