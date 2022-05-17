80b84f6cca8d3fdb09a1cc693df01949d0740bdd
Alderbane <alderban@outlook.com>
2022-05-16 22:10:55 -0500

    Update client.py

--- Proyecto/client.py
+++ Proyecto/client.py
@@ -14,6 +14,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(data)
     data = s.recv(1024)
-    print(data)
     eFile.write(data)
     
